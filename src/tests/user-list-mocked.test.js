50fe1eb233e6405aaea2929febd3fbefe5d32845
nikhilsapa5 <nikhilsapa5@gmail.com>
2022-12-06 00:45:50 -0500

    added files for likes branch

---
+++ src/tests/user-list-mocked.test.js
@@ -0,0 +1,33 @@
+import {UserList} from "../components/profile/user-list";
+import {screen, render} from "@testing-library/react";
+import React from 'react';
+import {HashRouter} from "react-router-dom";
+import {findAllUsers} from "../services/users-service";
+import axios from "axios";
+import 'regenerator-runtime/runtime' ;
+import '@testing-library/jest-dom/extend-expect';
+
+jest.mock('axios');
+
+const MOCKED_USERS = [
+  {username: 'ellen_ripley', password: 'lv426', email: 'repley@weyland.com', _id: "123"},
+  {username: 'sarah_conor', password: 'illbeback', email: 'sarah@bigjeff.com', _id: "234"},
+]
+
+// test rendering from Mock
+test('user list renders mocked', async () => {
+  axios.get.mockImplementation(() =>
+      Promise.resolve({ data: {users: MOCKED_USERS} }));
+  const response = await findAllUsers();
+  const users = response.users;
+
+  render(
+      <HashRouter>
+        <UserList users={users}/>
+      </HashRouter>);
+
+  const user = screen.getByText(/ellen_ripley/i);
+  expect(user).toBeInTheDocument();
+});
+
+//commenting
